4519101277e4f5379ed6010c5fd771ee3d586465
whysovague <fouad.anabosi@gmail.com>
2026-04-06 08:54:09 +0300

    completed todo 4

--- 6-3-node-express/backend/server.js
+++ 6-3-node-express/backend/server.js
@@ -5,7 +5,8 @@ const app = express();
 // TODO 1: Define server port
 const PORT = 3000;
 // TODO 4: Import and Apply CORS middleware
-
+import cors from "cors";
+app.use(cors());
 
 // TODO 5: Import and apply Morgan middleware
 
